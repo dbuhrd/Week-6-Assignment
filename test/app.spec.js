67871049bf1906396b4d8c53df2231f411519479
bethany ann <bethanyann44@gmail.com>
2022-03-17 16:00:30 -0500

    Update app.spec.js

--- test/app.spec.js
+++ test/app.spec.js
@@ -5,7 +5,7 @@ const { add } = require('../Week6_coding_assignment');
 describe('DeckOfCards', function() {
     describe('constructor', function(){
         it('should return a deck of cards', function(){
-            let aDeckofCards = new DeckofCards(); //why not defined?
+            let aDeckofCards = new DeckofCards(); //why not defined? - this isn't defined because DeckofCards needs to be DeckOfCards - capitalization matters in javascript! 
             expect(aDeckofCards).to.be.not(null);
 
 
